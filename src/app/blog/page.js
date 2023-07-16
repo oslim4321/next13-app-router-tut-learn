32e47f73393c4ada33026df17ad0f602f668a838
sooto selim <adewaleselim6@gmail.com>
2023-07-16 15:50:15 +0100

    worked on the description

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,7 +1,24 @@
+import { ImageComp } from "@/components/util";
+import Link from "next/link";
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="cont my-10">
+      <Link href="/blog/test" className="flex items-center gap-x-5 border">
+        <div>
+          <ImageComp src="/images/contact.png" className={"w-[200px]"} />
+        </div>
+        <div>
+          <h2 className="text-3xl">Lorem ipsum dolor sit amet.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+            accusantium?
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
