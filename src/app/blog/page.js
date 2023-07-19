0b93aba103559c29ced29a892e804adeb9e4dc74
sooto selim <adewaleselim6@gmail.com>
2023-07-19 19:03:12 +0100

    adding my mongo db link to blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await fetch("http://localhost:3000/api/post");
 
   if (!res.ok) {
     throw new Error("failed to fetch data");
@@ -13,24 +13,25 @@ async function getData() {
 
 const Blog = async () => {
   const data = await getData();
+  console.log(data);
   return (
     <div className="cont my-10">
       {data.map((post) => (
         <Link
-          href={`/blog/${post.id}`}
+          href={`/blog/${post._id}`}
           className="flex items-center gap-x-5 border my-5"
           key={post.id}
         >
-          <div>
+          {/* <div>
             <ImageComp
               src="/images/contact.png"
               className={"w-[200px]"}
               alt={post.title}
             />
-          </div>
+          </div> */}
           <div>
             <h2 className="text-3xl">{post.title}</h2>
-            <p>{post.body}</p>
+            <p>{post.content}</p>
           </div>
         </Link>
       ))}
