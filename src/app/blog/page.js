9808f58b63acefd9d41b9910673cddf7418137c9
sooto selim <adewaleselim6@gmail.com>
2023-07-31 14:53:36 +0100

    worked

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,46 +1,46 @@
-import { ImageComp } from "@/components/util";
-import Link from "next/link";
-import React from "react";
+// import { ImageComp } from "@/components/util";
+// import Link from "next/link";
+// import React from "react";
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/post");
-  if (!res.ok) {
-    throw new Error("failed to fetch data");
-  }
-  return res.json();
-}
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/api/post");
+//   if (!res.ok) {
+//     throw new Error("failed to fetch data");
+//   }
+//   return res.json();
+// }
 
-export const metadata = {
-  title: "Blog page",
-  description: "this is oslim my page blog site",
-};
+// export const metadata = {
+//   title: "Blog page",
+//   description: "this is oslim my page blog site",
+// };
 
-const Blog = async () => {
-  const data = await getData();
-  console.log(data);
-  return (
-    <div className="cont my-10">
-      {data.map((post) => (
-        <Link
-          href={`/blog/${post._id}`}
-          className="flex items-center gap-x-5 border my-5"
-          key={post.id}
-        >
-          <div>
-            <ImageComp
-              src={post.image}
-              className={"w-[200px]"}
-              alt={post.title}
-            />
-          </div>
-          <div>
-            <h2 className="text-3xl">{post.title}</h2>
-            <p>{post.content}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
-};
+// const Blog = async () => {
+//   const data = await getData();
+//   console.log(data);
+//   return (
+//     <div className="cont my-10">
+//       {data.map((post) => (
+//         <Link
+//           href={`/blog/${post._id}`}
+//           className="flex items-center gap-x-5 border my-5"
+//           key={post.id}
+//         >
+//           <div>
+//             <ImageComp
+//               src={post.image}
+//               className={"w-[200px]"}
+//               alt={post.title}
+//             />
+//           </div>
+//           <div>
+//             <h2 className="text-3xl">{post.title}</h2>
+//             <p>{post.content}</p>
+//           </div>
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default Blog;
+// export default Blog;
--- src/app/dashboard/(auth)/login/page.js
+++ src/app/dashboard/(auth)/login/page.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Login from "./Login";
+// import React from "react";
+// import Login from "./Login";
 
-const page = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
-};
+// const page = () => {
+//   return (
+//     <div>
+//       <Login />
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;
