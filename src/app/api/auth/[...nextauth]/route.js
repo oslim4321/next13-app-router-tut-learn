408b32bcbb6cf395ebe6a9776feac6ca9884ad68
sooto selim <adewaleselim6@gmail.com>
2023-07-31 15:41:38 +0100

    hmm

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,46 +1,46 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import connect from "@/utils/db";
-import Users from "@/models/Users";
-import bcrypt from "bcryptjs";
-const handler = NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.Goggle_Client_Id,
-      clientSecret: process.env.Goggle_Client_Secret,
-    }),
-    // ...add more providers here
-    CredentialsProvider({
-      id: "credentials",
-      name: "credentials",
-      async authorize(credentials) {
-        await connect();
-        try {
-          const user = await Users.findOne({ email: credentials.email });
-          if (user) {
-            const isPasswordCorrect = await bcrypt.compare(
-              credentials.password,
-              user.password
-            );
-            if (isPasswordCorrect) {
-              return user;
-            } else {
-              throw new Error("wrong credentials");
-            }
-          } else {
-            throw new Error("User not found");
-          }
-        } catch (error) {
-          throw new Error(error);
-        }
-      },
-    }),
-  ],
-  pages: {
-    error: "/dashboard/login",
-  },
-});
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import connect from "@/utils/db";
+// import Users from "@/models/Users";
+// import bcrypt from "bcryptjs";
+// const handler = NextAuth({
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.Goggle_Client_Id,
+//       clientSecret: process.env.Goggle_Client_Secret,
+//     }),
+//     // ...add more providers here
+//     CredentialsProvider({
+//       id: "credentials",
+//       name: "credentials",
+//       async authorize(credentials) {
+//         await connect();
+//         try {
+//           const user = await Users.findOne({ email: credentials.email });
+//           if (user) {
+//             const isPasswordCorrect = await bcrypt.compare(
+//               credentials.password,
+//               user.password
+//             );
+//             if (isPasswordCorrect) {
+//               return user;
+//             } else {
+//               throw new Error("wrong credentials");
+//             }
+//           } else {
+//             throw new Error("User not found");
+//           }
+//         } catch (error) {
+//           throw new Error(error);
+//         }
+//       },
+//     }),
+//   ],
+//   pages: {
+//     error: "/dashboard/login",
+//   },
+// });
 
-export { handler as POST, handler as GET };
+// export { handler as POST, handler as GET };
--- src/app/api/post/[id]/route.js
+++ src/app/api/post/[id]/route.js
@@ -1,29 +1,29 @@
-import Post from "@/models/Post";
-import connect from "@/utils/db";
-import { NextResponse } from "next/server";
+// import Post from "@/models/Post";
+// import connect from "@/utils/db";
+// import { NextResponse } from "next/server";
 
-export const GET = async (request, { params }) => {
-  const { id } = params;
-  console.log(id);
-  try {
-    const post = await Post.findById(id);
-    console.log(post);
-    return new NextResponse(JSON.stringify(post), { status: 200 });
-    // await connect();
-  } catch (error) {
-    return new NextResponse("database error");
-  }
-};
+// export const GET = async (request, { params }) => {
+//   const { id } = params;
+//   console.log(id);
+//   try {
+//     const post = await Post.findById(id);
+//     console.log(post);
+//     return new NextResponse(JSON.stringify(post), { status: 200 });
+//     // await connect();
+//   } catch (error) {
+//     return new NextResponse("database error");
+//   }
+// };
 
-export const DELETE = async (request, { params }) => {
-  console.log(params, "delete");
-  const { id } = params;
-  try {
-    await connect();
-    await Post.findByIdAndDelete(id);
-    console.log("item deleted");
-    return new NextResponse("item deleted success");
-  } catch (error) {
-    console.log(error);
-  }
-};
+// export const DELETE = async (request, { params }) => {
+//   console.log(params, "delete");
+//   const { id } = params;
+//   try {
+//     await connect();
+//     await Post.findByIdAndDelete(id);
+//     console.log("item deleted");
+//     return new NextResponse("item deleted success");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
